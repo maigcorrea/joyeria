a033a70e721194b7f07241f4f624ce8e77699031
Maite <anamaitegarciacorrea@gmail.com>
2025-08-24 13:12:27 +0200

    prueba

--- src/hooks/usePageTracking.jsx
+++ src/hooks/usePageTracking.jsx
@@ -1,12 +1,14 @@
-// src/hooks/usePageTracking.js
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export function usePageTracking(gaId) {
-  const { pathname } = useLocation();
+  const location = useLocation();
 
   useEffect(() => {
     if (!gaId || !window.gtag) return;
-    window.gtag("event", "page_view", { page_path: pathname });
-  }, [gaId, pathname]);
+
+    window.gtag("config", gaId, {
+      page_path: location.pathname,
+    });
+  }, [location, gaId]);
 }
