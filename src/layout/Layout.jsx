2c2663921a5169981db7b14ead7d117a5cedda0d
Maite <anamaitegarciacorrea@gmail.com>
2025-08-24 12:06:20 +0200

    Ajustes Vercel

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import FloatingNavbar from '../components/navBar'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const Layout = () => {
   return (
     <>
+    <SpeedInsights >
         <header>
             <FloatingNavbar />
         </header>
         <main>
             <Outlet></Outlet>
         </main>
+        </SpeedInsights>
     </>
   )
 }
