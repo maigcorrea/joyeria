c8f515bebab2d0ff6bf777e5faf959690fd54677
Maite <anamaitegarciacorrea@gmail.com>
2025-08-24 13:10:55 +0200

    prueba

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import FloatingNavbar from '../components/navBar'
 import { SpeedInsights } from "@vercel/speed-insights/react"
