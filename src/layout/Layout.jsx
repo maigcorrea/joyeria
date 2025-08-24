a542f89212414c241e5f922fdf6b796c277fac4c
Maite <anamaitegarciacorrea@gmail.com>
2025-08-24 13:04:27 +0200

    Quito analiticas porque da fallo

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,29 +4,29 @@ import FloatingNavbar from '../components/navBar'
 import { SpeedInsights } from "@vercel/speed-insights/react"
 // import { usePageTracking } from '../hooks/usePageTracking'
 
-const GA_ID = import.meta.env.VITE_GA_ID;
+// const GA_ID = import.meta.env.VITE_GA_ID;
 
 const Layout = () => {
-  useEffect(() => {
-    // usePageTracking(GA_ID);
+  // useEffect(() => {
+  //   // usePageTracking(GA_ID);
 
-    if (!GA_ID) return;
+  //   if (!GA_ID) return;
 
-    // Inyectar script de Google Analytics
-    const script1 = document.createElement("script");
-    script1.async = true;
-    script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
-    document.head.appendChild(script1);
+  //   // Inyectar script de Google Analytics
+  //   const script1 = document.createElement("script");
+  //   script1.async = true;
+  //   script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+  //   document.head.appendChild(script1);
 
-    const script2 = document.createElement("script");
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${GA_ID}');
-    `;
-    document.head.appendChild(script2);
-  }, []);
+  //   const script2 = document.createElement("script");
+  //   script2.innerHTML = `
+  //     window.dataLayer = window.dataLayer || [];
+  //     function gtag(){dataLayer.push(arguments);}
+  //     gtag('js', new Date());
+  //     gtag('config', '${GA_ID}');
+  //   `;
+  //   document.head.appendChild(script2);
+  // }, []);
   return (
     <>
         <header>
