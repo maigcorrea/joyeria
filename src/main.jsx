89db40197c721f32c8bfc0d1ac81b181dfa398c5
Maite <anamaitegarciacorrea@gmail.com>
2025-08-26 13:19:36 +0200

    Personalización de scroll

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Lenis from '@studio-freight/lenis'
+
+// Inicializa Lenis
+const lenis = new Lenis({
+    duration: 1.8, // más alto = scroll más lento
+    easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)), // curva de easing
+    smoothWheel: true,
+    smoothTouch: false,
+  })
+  
+  // Animación en cada frame
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  
+  requestAnimationFrame(raf)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
